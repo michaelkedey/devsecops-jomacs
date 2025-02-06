9f6971e97ab31271519e56579e0353fd44daf51a
michaelkedey <privatemyk@gmail.com>
2025-02-06 14:05:04 +0000

    infra9

---
+++ infra/aws/modules/vpc/output.tf
@@ -0,0 +1,15 @@
+output "pb_sn1" {
+  value = aws_subnet.project_public_subnet.id
+}
+
+output "pr_sn" {
+  value = aws_subnet.project_private_subnet.id
+}
+
+output "vpc" {
+  value = aws_vpc.project_vpc.id
+}
+
+output "instance_sg" {
+  value = aws_security_group.project_instance_sg.id
+}

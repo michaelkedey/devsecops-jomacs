5851ad57de42c62ab6f4d527c2e8c02e17e59013
michaelkedey <privatemyk@gmail.com>
2025-04-09 18:55:49 +0000

    infra026

--- app/python/app.py
+++ app/python/app.py
@@ -11,17 +11,32 @@
 #     app.run(debug=False)
 
 
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# @app.route('/app')  # Add the /app route to handle requests to /app
+# def app_page():
+#     return render_template('index.html')  # Or you can return a different template/content for /app
+
+# if __name__ == '__main__':
+#     app.run(debug=False)
+
+
 from flask import Flask, render_template
 
+# Initialize the Flask application
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/app')  # This line makes /app also map to the home() function
 def home():
     return render_template('index.html')
 
-@app.route('/app')  # Add the /app route to handle requests to /app
-def app_page():
-    return render_template('index.html')  # Or you can return a different template/content for /app
-
 if __name__ == '__main__':
     app.run(debug=False)
+

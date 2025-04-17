4d1e035e8e259ee14eebead327621052677ec041
michaelkedey <privatemyk@gmail.com>
2025-04-17 19:58:21 +0000

    elk33

--- app/python/app.py
+++ app/python/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/app')  # This line makes /app also map to the home() function
 def home():
     return render_template('index.html')
 

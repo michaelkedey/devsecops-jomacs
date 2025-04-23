716ef2398b54ccf1e5c43d6e398bde4405b9f924
michaelkedey <privatemyk@gmail.com>
2025-04-23 11:17:16 +0000

    final-fix008

--- infra/aws/modules/secrets_manager/secrets.tf
+++ infra/aws/modules/secrets_manager/secrets.tf
@@ -1,5 +1,8 @@
 resource "aws_secretsmanager_secret" "jp_elastic_password" {
   name = var.secrets_name
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_secretsmanager_secret_version" "elastic_password_value" {

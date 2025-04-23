523962de16d977d4cd722d501704918f0819c214
michaelkedey <privatemyk@gmail.com>
2025-04-23 11:45:44 +0000

    final-fix009

--- infra/aws/modules/secrets_manager/secrets.tf
+++ infra/aws/modules/secrets_manager/secrets.tf
@@ -1,11 +1,24 @@
+resource "aws_kms_key" "elk_secrets_key" {
+  description             = "KMS key for ELK secrets encryption"
+  deletion_window_in_days = var.deletion_window
+  enable_key_rotation     = true
+}
+
 resource "aws_secretsmanager_secret" "jp_elastic_password" {
   name = var.secrets_name
+  kms_key_id = aws_kms_key.elk_secrets_key.arn
   lifecycle {
     prevent_destroy = true
+    ignore_changes  = [tags]
   }
 }
 
-resource "aws_secretsmanager_secret_version" "elastic_password_value" {
+resource "aws_secretsmanager_secret_version" "jp_elastic_password_value" {
   secret_id     = aws_secretsmanager_secret.jp_elastic_password.id
   secret_string = var.tmp_elastic_password
+
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
 }
+
--- infra/aws/modules/secrets_manager/variables.tf
+++ infra/aws/modules/secrets_manager/variables.tf
@@ -7,3 +7,7 @@ variable "tmp_elastic_password" {
   type    = string
   default = "tmp_elastic_password"
 }
+variable "deletion_window" {
+  default = 7
+  type = number
+}
